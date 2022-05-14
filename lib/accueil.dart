b15e434d51d27bb79cf7c0102434b5204a6bd37b
Hugo PANEL <hugo.panel@efrei.net>
2022-05-14 21:33:54 +0200

    Transfer (test)

---
+++ lib/accueil.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'customCardWidget.dart';
+
+class Accueil extends StatelessWidget {
+  const Accueil({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(title),
+      ),
+      child: ListView(
+        // padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              child: const Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  "Bienvenue",
+                  style: TextStyle(fontSize: 35.0),
+                ),
+              ),
+            ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    customCardWidget(
+                        title: "Aujourd'hui",
+                        content:
+                        "Rappels pour aujourd'hui.\nEvènements à venir aujourd'hui...",
+                        action: TextButton(
+                            onPressed: () {},
+                            child: Text("Mon calendrier"))),
+                    customCardWidget(
+                        title: "Demain",
+                        content:
+                        "Rappels pour demain.\nEvènements à venir demain...",
+                        action: TextButton(
+                            onPressed: () {},
+                            child: Text("Mes tâches à faire"))),
+                    customCardWidget(
+                        title: "Plus tard",
+                        content:
+                        "Rappels pour plus tard.\nEvènements à venir plus tard...",
+                        action: TextButton(
+                            onPressed: () {},
+                            child: Text("Mon calendrier"))),
+                    SizedBox(height: 50),
+                    customCardWidget(title: "Le saviez-vous ?",content: "Petite info croustillante ici."),
+                    SizedBox(height: 50),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                    customCardWidget(title: "Titre",content: "Texte à afficher."),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-import 'customCardWidget.dart';
+import 'accueil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,16 +16,14 @@ class MyApp extends StatelessWidget {
     return const CupertinoApp(
         title: 'Flutter Demo',
         theme: CupertinoThemeData(brightness: Brightness.light),
-        home: CupertinoPage(
-          title: 'Accueil',
-        ));
+        home: CupertinoPage());
   }
 }
 
+// TODO: Se renseigner sur les States.
 class CupertinoPage extends StatelessWidget {
-  const CupertinoPage({Key? key, this.title = "Default title"})
+  const CupertinoPage({Key? key})
       : super(key: key);
-  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -43,69 +41,7 @@ class CupertinoPage extends StatelessWidget {
         ]),
         tabBuilder: (BuildContext context, int index) {
           return CupertinoTabView(builder: (BuildContext context) {
-            return CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                middle: Text(title),
-              ),
-              child: ListView(
-                  // padding: const EdgeInsets.all(8),
-                  children: <Widget>[
-                    Container(
-                      child: const Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Text(
-                          "Bienvenue",
-                          style: TextStyle(fontSize: 35.0),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: Column(
-                          children: [
-                            customCardWidget(
-                                title: "Aujourd'hui",
-                                content:
-                                    "Rappels pour aujourd'hui.\nEvènements à venir aujourd'hui...",
-                                action: TextButton(
-                                    onPressed: () {},
-                                    child: Text("Mon calendrier"))),
-                            customCardWidget(
-                                title: "Demain",
-                                content:
-                                "Rappels pour demain.\nEvènements à venir demain...",
-                                action: TextButton(
-                                    onPressed: () {},
-                                    child: Text("Mes tâches à faire"))),
-                            customCardWidget(
-                                title: "Plus tard",
-                                content:
-                                "Rappels pour plus tard.\nEvènements à venir plus tard...",
-                                action: TextButton(
-                                    onPressed: () {},
-                                    child: Text("Mon calendrier"))),
-                            SizedBox(height: 50),
-                            customCardWidget(title: "Le saviez-vous ?",content: "Petite info croustillante ici."),
-                            SizedBox(height: 50),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                            customCardWidget(title: "Titre",content: "Texte à afficher."),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ]),
-            );
+            return Accueil(key: key, title: "Accueil");
           });
         });
   }
